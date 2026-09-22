85f156d1b16b8996ea78aea44156575cab4d7ee1
cooc <minhdaonguyenad@gmail.com>
2026-09-22 17:19:25 +0700

    refactor: xoa bo demo cu sau khi dong bo WEBNC

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-
-const PORT = process.env.PORT || 3000;
-const HOST = "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-  console.log(`WEBNC2 running at http://localhost:${PORT}`);
-});
