987bf8188bc4c1e000a10d5f5c58c042ee388254
cooc <minhdaonguyenad@gmail.com>
2026-09-22 17:19:02 +0700

    fix: giu nguyen hanh vi API cua WEBNC

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -3,12 +3,19 @@ const path = require("path");
 
 const DB_FILE = path.join(__dirname, "..", "data", "db.json");
 
-async function news(req, res, next) {
+async function news(req, res) {
   try {
     const data = await fs.readFile(DB_FILE, "utf8");
-    res.json(JSON.parse(data));
+    const posts = JSON.parse(data);
+
+    res
+      .status(200)
+      .type("application/json")
+      .send(JSON.stringify(posts, null, 2));
   } catch (error) {
-    next(error);
+    res.status(500).json({
+      error: "Khong the doc database"
+    });
   }
 }
 
